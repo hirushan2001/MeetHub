83773af471d9c0180725a41fb712f1de330a8a6f
hirushan2001 <135137760+hirushan2001@users.noreply.github.com>
2024-09-06 22:28:08 +0530

    navbar and mobile navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+import MobileNav from './MobileNav'
+
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className='flex flex-between fixed  z-50 w-full p-6 py-4 lg:px-10  bg-dark-1'>
+      <Link
+        href='/'
+        className='flex items-center gap-1'
+        >
+        <Image
+        src="/icons/logo.svg"
+        alt="MeetHub Logo"
+        width={32}
+        height={32}
+        className='max-sm:size-10'
+        />
+        <p className='text-[26px] font-extrabold text-white max-sm:hidden'>MeetHUB</p>
+        </Link>
+
+        <div className='flex-between  gap-5'>
+           <MobileNav/>
+        </div>
+    </nav>
   )
 }
 
