cec5ae0753ea310cadd1a9275e8b7d58df5d5095
hirushan2001 <135137760+hirushan2001@users.noreply.github.com>
2024-09-08 09:06:42 +0530

    Popup Window

--- components/ui/MeetingTypeList.tsx
+++ components/ui/MeetingTypeList.tsx
@@ -4,17 +4,22 @@ import Image from 'next/image'
 import { useState } from 'react'
 import HomeCard from './HomeCard'
 import { useRouter } from 'next/navigation'
+import MeetingModal from './MeetingModal'
 
 const MeetingTypeList = () => {
     const router = useRouter()
     const [meetingState, setMeetingState] = useState<'isSheduleMeeting' | 'isJoiningMeeting' | 'isInstantMeeting' | undefined>()
+
+    const createMeeting = () => {
+      
+    }
   return (
     <section className='grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4'>
        <HomeCard
        img="/icons/add-meeting.svg"
        title="New Meeting"
        description="Start am instant meeting"
-       handleClick={() => setMeetingState('isJoiningMeeting')}
+       handleClick={() => setMeetingState('isInstantMeeting')}
        className="bg-orange-1"
        />
 
@@ -43,6 +48,16 @@ const MeetingTypeList = () => {
        handleClick={() => setMeetingState('isJoiningMeeting')}
        className="bg-yellow-1"
        />
+
+       <MeetingModal 
+        isOpen={meetingState === 'isInstantMeeting'}
+        onClose={() => setMeetingState(undefined)}
+        title="Start Meeting"
+        className="text-center"
+        buttonText="Start Meeting"
+        handleClick={createMeeting} 
+        />
+
     </section>
   )
 }
