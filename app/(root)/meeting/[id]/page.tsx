887bb144d20e62d76603d439de40baf78ba8722c
hirushan2001 <135137760+hirushan2001@users.noreply.github.com>
2024-09-09 12:19:28 +0530

    update

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -1,8 +1,29 @@
-import React from 'react'
+'use client'
 
-const Meeting = ({params}:{params:{ id: string } }) => {
+import MeetingRoom from '@/components/ui/MeetingRoom';
+import MeetingSetup from '@/components/ui/MeetingSetup';
+import { useGetCallById } from '@/hooks/useGetcallById';
+import { useUser } from '@clerk/nextjs'
+import { StreamCall, StreamTheme } from '@stream-io/video-react-sdk'
+import { Loader } from 'lucide-react';
+import React, { useState } from 'react'
+
+const Meeting = ({params:{ id }}:{params:{ id: string } }) => {
+
+  const {user , isLoaded} = useUser();
+  const [isSetupComplete, setIsSetupComplete] = useState(false);
+  const { call , isCallLoading } = useGetCallById(id);
+
+  if(!isLoaded || !isCallLoading) return <Loader />
   return (
-    <div>Meeting Room: #{params.id}</div>
+    
+    <main className='h-screen w-full '>
+    <StreamCall call={call}>
+      <StreamTheme>
+        {!isSetupComplete ? (<MeetingSetup/>) : (<MeetingRoom />)}
+      </StreamTheme>
+    </StreamCall>
+    </main>
   )
 }
 
